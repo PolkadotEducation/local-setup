436095eaea0f6549dd6d540fa415b3d6194f0713
Artur Gontijo <arturgontijo@gmail.com>
2024-07-24 18:50:12 -0300

    [v0.1] run.sh + docker-compose.yaml

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# local-setup
-Quickstart local development setup
+# Quickstart local development setup
+
+### 1 - Clone this repo
+```
+git clone https://github.com/PolkadotEducation/local-setup.git
+cd local-setup
+```
+
+### 2 - Clone all other repos
+```
+./run.sh
+```
+
+### 3 - Run Docker compose
+```
+docker compose up
+```
+
+### 4 - Shutdown Docker compose
+```
+docker compose down
+```
---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+BRANCH_NAME=${1:-main}
+CODEBASE_DIR="codebase"
+REPOSITORIES=("api" "app" "lp")
+
+if [ ! -d ${CODEBASE_DIR} ]; then
+  mkdir ${CODEBASE_DIR}
+fi
+
+cd ${CODEBASE_DIR}
+
+for REPOSITORY in "${REPOSITORIES[@]}"; do
+  if [ -d ${REPOSITORY} ]; then
+    cd ${REPOSITORY} && git pull origin $BRANCH_NAME && cd ..
+  else
+    branch_name=$BRANCH_NAME
+    git clone -b $branch_name https://github.com/PolkadotEducation/${REPOSITORY}.git
+  fi
+done
